2eceb64d4bf1ab3c99398fe27484895ecf187ac8
nitarula <nitarula@WKWZTcYYyNvQR3A.global.publicisgroupe.net>
2022-06-19 14:28:10 +0530

    true monochrome

--- MapsApiFramework/src/test/java/cucumber/options/TestRunner.java
+++ MapsApiFramework/src/test/java/cucumber/options/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src\\test\\resources\\Features\\addPlaceFeature.feature"},
 					glue = {"stepDef"},
-					monochrome = false,
+					monochrome = true,
 					plugin = {"pretty","html:target/cucumber-reports.html"})
 public class TestRunner {
 
